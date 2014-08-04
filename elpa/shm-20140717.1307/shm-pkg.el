8450848b278d45dce2a93de731f86937c7641c8e
Michael Xavier <michael@michaelxavier.net>
2014-08-04 08:51:29 -0700

    upgrade packages

--- elpa/shm-20140717.1307/shm-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "shm" "20140717.1307" "Structured Haskell Mode" 'nil :keywords
-  '("development" "haskell" "structured"))
---
+++ elpa/shm-20140804.151/shm-pkg.el
@@ -0,0 +1,2 @@
+(define-package "shm" "20140804.151" "Structured Haskell Mode" 'nil :keywords
+  '("development" "haskell" "structured"))
