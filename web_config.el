3b302477847abecc151fbd3e8ddace7109ddd682
Michael Xavier <michael@michaelxavier.net>
2014-09-09 12:41:41 -0700

    Recognize tpl files

--- web_config.el
+++ web_config.el
@@ -1,6 +1,8 @@
 ;; web-mode stuff
+;;TODO: DRY
 (add-to-list 'auto-mode-alist '(".html$" . web-mode))
 (add-to-list 'auto-mode-alist '(".eco$" . web-mode))
 (add-to-list 'auto-mode-alist '(".jst$" . web-mode))
 (add-to-list 'auto-mode-alist '(".liquid$" . web-mode))
 (add-to-list 'auto-mode-alist '(".html.erb$" . web-mode))
+(add-to-list 'auto-mode-alist '(".tpl$" . web-mode))
