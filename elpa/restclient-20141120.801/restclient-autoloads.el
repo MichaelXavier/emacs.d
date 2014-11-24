b5d5bac15a91c677ea0affd370cabf01e2403b2e
Michael Xavier <michael@michaelxavier.net>
2014-11-23 19:08:15 -0800

    install restclient

---
+++ elpa/restclient-20141120.801/restclient-autoloads.el
@@ -0,0 +1,40 @@
+;;; restclient-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (restclient-mode restclient-http-send-current-raw
+;;;;;;  restclient-http-send-current) "restclient" "restclient.el"
+;;;;;;  (21614 45687 452922 76000))
+;;; Generated autoloads from restclient.el
+
+(autoload 'restclient-http-send-current "restclient" "\
+
+
+\(fn &optional RAW)" t nil)
+
+(autoload 'restclient-http-send-current-raw "restclient" "\
+
+
+\(fn)" t nil)
+
+(autoload 'restclient-mode "restclient" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("restclient-pkg.el") (21614 45687 473513
+;;;;;;  220000))
+
+;;;***
+
+(provide 'restclient-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; restclient-autoloads.el ends here
---
+++ elpa/restclient-20141120.801/restclient-pkg.el
@@ -0,0 +1,1 @@
+(define-package "restclient" "20141120.801" "An interactive HTTP client for Emacs" (quote ((json-reformat "0.0.1"))))
