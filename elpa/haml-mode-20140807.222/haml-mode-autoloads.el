a21e496fde64b2e320f0eeaa3424ff9852916f31
Michael Xavier <michael@michaelxavier.net>
2014-11-26 16:05:02 -0800

    update all the things

--- elpa/haml-mode-20140807.222/haml-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; haml-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (haml-mode) "haml-mode" "haml-mode.el" (21490 22055
-;;;;;;  944962 387000))
-;;; Generated autoloads from haml-mode.el
-
-(autoload 'haml-mode "haml-mode" "\
-Major mode for editing Haml files.
-
-\\{haml-mode-map}
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.haml\\'" . haml-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("haml-mode-pkg.el") (21490 22056 48061
-;;;;;;  615000))
-
-;;;***
-
-(provide 'haml-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; haml-mode-autoloads.el ends here
--- elpa/restclient-20141120.801/restclient-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "restclient" "20141120.801" "An interactive HTTP client for Emacs" (quote ((json-reformat "0.0.1"))))
