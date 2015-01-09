f0f4784e7627546559793a18c9f071737e0a4055
Michael Xavier <michael@michaelxavier.net>
2015-01-09 10:25:51 -0800

    tslint checker

--- typescript_config.el
+++ typescript_config.el
@@ -1,2 +1,12 @@
 ;; typescript configurations
 (add-to-list 'auto-mode-alist '(".ts$" . typescript-mode))
+(require 'flycheck)
+(flycheck-define-checker tslint
+  "Use tslint to flycheck TypeScript code."
+  :command ("tslint"
+            "-f" source
+            "-c" (eval (projectile-expand-root "tslint.json"))
+            "-t" "prose")
+  :error-patterns ((warning (file-name) "[" line ", " column "]: " (message)))
+  :modes typescript-mode)
+(add-to-list 'flycheck-checkers 'tslint)
