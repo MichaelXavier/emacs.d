0d6b05b721efd16c30cd51571db7ed2cc59435a4
Michael Xavier <michael@michaelxavier.net>
2015-02-15 12:23:51 -0800

    Cask

---
+++ .cask/24.3.1/bootstrap/commander-20140120.1052/commander-autoloads.el
@@ -0,0 +1,18 @@
+;;; commander-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("commander-pkg.el" "commander.el") (21710
+;;;;;;  26351 763607 670000))
+
+;;;***
+
+(provide 'commander-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; commander-autoloads.el ends here
---
+++ .cask/24.3.1/bootstrap/git-20140128.241/git-autoloads.el
@@ -0,0 +1,18 @@
+;;; git-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("git-pkg.el" "git.el") (21710 26351 980661
+;;;;;;  963000))
+
+;;;***
+
+(provide 'git-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; git-autoloads.el ends here
