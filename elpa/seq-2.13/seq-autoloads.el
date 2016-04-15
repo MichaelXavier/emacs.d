bc8ff51fb0eb47d9750e7aad79736625eaa7d4ba
Michael Xavier <michael@michaelxavier.net>
2016-04-15 13:27:06 -0700

    update deps

--- elpa/seq-2.13/seq-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; seq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("seq-24.el" "seq-25.el" "seq-pkg.el" "seq.el")
-;;;;;;  (22265 18752 801814 906000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; seq-autoloads.el ends here
