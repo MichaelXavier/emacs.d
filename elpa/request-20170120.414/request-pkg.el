58af498d8a9bbf400b48f1d9ecb15f6f5f3baac9
Michael Xavier <michael@michaelxavier.net>
2017-02-08 08:18:43 -0800

    update deps

--- elpa/request-20170120.414/request-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "request" "20170120.414" "Compatible layer for URL request in Emacs" '((emacs "24") (cl-lib "0.5")))
--- elpa/web-mode-20170118.1211/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20170118.1211" "major mode for editing web templates" 'nil :url "http://web-mode.org" :keywords '("languages"))
