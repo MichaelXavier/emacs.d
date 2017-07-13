92a5dca6485743bd9e48faa99f6200a714c73fd9
Michael Xavier <michael@michaelxavier.net>
2017-07-12 18:27:19 -0700

    update deps

--- elpa/w3m-20170705.1139/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20170705.1139" "an Emacs interface to w3m" 'nil :keywords
-  '("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/web-mode-20170613.959/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20170613.959" "major mode for editing web templates" '((emacs "23.1")) :commit "8c35710775de9369f0637a9bd5fde39e4074db72" :url "http://web-mode.org" :keywords '("languages"))
