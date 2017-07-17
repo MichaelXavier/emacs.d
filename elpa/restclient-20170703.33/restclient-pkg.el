7830e2af43d990c7aa8b78121968320ac2c4c0bb
Michael Xavier <michael@michaelxavier.net>
2017-07-17 08:46:11 -0700

    update deps

--- elpa/restclient-20170703.33/restclient-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "restclient" "20170703.33" "An interactive HTTP client for Emacs" 'nil :commit "d8bb3fbfea7e59587bf746c45c655fdf94af3247" :keywords '("http"))
--- elpa/w3m-20170711.743/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20170711.743" "an Emacs interface to w3m" 'nil :keywords
-  '("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
