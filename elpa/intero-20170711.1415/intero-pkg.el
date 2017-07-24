26a4f13cc24aff34079bee3eb33c93089fb4e8eb
Michael Xavier <michael@michaelxavier.net>
2017-07-24 15:37:33 -0700

    update deps

--- elpa/intero-20170711.1415/intero-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "intero" "20170711.1415" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "00f74d048ad1b1879ceda814a17b98b8982f089e" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
---
+++ elpa/projectile-20170724.442/projectile-pkg.el
@@ -0,0 +1,1 @@
+(define-package "projectile" "20170724.442" "Manage and navigate projects in Emacs easily" '((emacs "24.1") (pkg-info "0.4")) :commit "7951b17e6d4adef8fcb4e15ef6c466ef53b59ccf" :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))
