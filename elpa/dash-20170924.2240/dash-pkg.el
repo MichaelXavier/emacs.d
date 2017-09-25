1a4ec677cf2ebc35029c280c9e32c0b3035bef5a
Michael Xavier <michael@michaelxavier.net>
2017-09-25 08:50:10 -0700

    update deps

---
+++ elpa/dash-20170924.2240/dash-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dash" "20170924.2240" "A modern list library for Emacs" 'nil :commit "c026c46527686edeee94fbb7da5a42afcadbc6bd" :keywords '("lists"))
---
+++ elpa/markdown-mode-20170924.1720/markdown-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "markdown-mode" "20170924.1720" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "03589677aa4ef48be659bfe7199c88dbc2f6c32b" :url "https://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
