bbe1d5f04c95bfd0269eaec83d1d58dd482c4f21
Michael Xavier <michael@michaelxavier.net>
2017-10-30 16:27:00 -0700

    update deps

---
+++ elpa/ht-20171024.1200/ht-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ht" "20171024.1200" "The missing hash table library for Emacs" '((dash "2.12.0")) :commit "a741bac82797d25ec0542764df7443e9e7241033" :keywords '("hash table" "hash map" "hash"))
