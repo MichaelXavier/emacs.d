b8c351ba71f46b430e5e7508b5b9ece1024f75d7
Michael Xavier <michael@michaelxavier.net>
2017-12-27 08:33:24 -0800

    update deps

--- elpa/intero-20171215.332/intero-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "intero" "20171215.332" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "ca8b99ab55a34c376ad6746ba0d30a590af18a48" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
