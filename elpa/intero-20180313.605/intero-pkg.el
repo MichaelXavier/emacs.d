83f8d594026c5df86a77aca68d8e478fdd54c3d1
Michael Xavier <michael@michaelxavier.net>
2018-03-22 10:59:29 -0700

    update deps

---
+++ elpa/intero-20180313.605/intero-pkg.el
@@ -0,0 +1,1 @@
+(define-package "intero" "20180313.605" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "ebcf8a0935b57eb54351b78ed8c358cbef96f4bd" :url "https://github.com/commercialhaskell/intero" :keywords '("haskell" "tools"))
---
+++ elpa/tuareg-20180312.410/tuareg-pkg.el
@@ -0,0 +1,7 @@
+(define-package "tuareg" "20180312.410" "OCaml mode for Emacs."
+  '((caml "3.12.0.1"))
+  :url "https://github.com/ocaml/tuareg" :keywords
+  '("ocaml" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
