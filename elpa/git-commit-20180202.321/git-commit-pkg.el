e9dd6fe6b9fee2e8e7537d7be3e37e6c190d8de6
Michael Xavier <michael@michaelxavier.net>
2018-04-02 09:01:32 -0700

    update deps

--- elpa/git-commit-20180202.321/git-commit-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "git-commit" "20180202.321" "Edit Git commit messages" '((emacs "24.4") (dash "20170810") (with-editor "20170817")) :commit "1c4fa9b144a273a08d5c910a528ede6bf27125b6" :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
