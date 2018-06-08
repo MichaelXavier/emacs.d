b58474c02520f670ae16e85cf7a99192ca1b61a7
Michael Xavier <michael@michaelxavier.net>
2018-06-07 19:48:10 -0700

    update deps

---
+++ elpa/markdown-mode-20180525.1952/markdown-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "markdown-mode" "20180525.1952" "Major mode for Markdown-formatted text" '((emacs "24.4") (cl-lib "0.5")) :commit "4f9c5e24aec582ed3413a8165ec0d613e71b7430" :keywords '("markdown" "github flavored markdown" "itex") :url "https://jblevins.org/projects/markdown-mode/")
--- elpa/psc-ide-20180404.215/psc-ide-autoloads.el
+++
@@ -1,43 +0,0 @@
-;;; psc-ide-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "psc-ide" "psc-ide.el" (23239 65286 939711
-;;;;;;  714000))
-;;; Generated autoloads from psc-ide.el
-
-(autoload 'psc-ide-mode "psc-ide" "\
-psc-ide-mode definition
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil "psc-ide-flycheck" "psc-ide-flycheck.el" (23239
-;;;;;;  65286 927711 461000))
-;;; Generated autoloads from psc-ide-flycheck.el
-
-(autoload 'psc-ide-flycheck-insert-suggestion "psc-ide-flycheck" "\
-Replace error with suggestion from psc compiler.
-
-\(fn)" t nil)
-
-(autoload 'psc-ide-flycheck-setup "psc-ide-flycheck" "\
-Setup Flycheck purescript.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("psc-ide-backported.el" "psc-ide-pkg.el"
-;;;;;;  "psc-ide-protocol.el") (23239 65286 981256 296000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; psc-ide-autoloads.el ends here
