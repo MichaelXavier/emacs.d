b2cf7a09e35b8d287f1b12488482b6ba2ff9862b
Michael Xavier <michael@michaelxavier.net>
2018-07-16 08:42:03 -0700

    update deps

---
+++ elpa/ess-20180713.1416/ess-pkg.el
@@ -0,0 +1,4 @@
+(define-package "ess" "20180713.1416" "Emacs Speaks Statistics" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/wgrep-20180710.2326/wgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "wgrep" "20180710.2326" "Writable grep buffer and apply the changes to files" 'nil)
