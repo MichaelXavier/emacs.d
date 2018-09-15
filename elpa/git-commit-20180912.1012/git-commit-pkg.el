cb22439e6b56ecb8427bb3c1451e82880b83d348
Michael Xavier <michael@michaelxavier.net>
2018-09-14 17:56:30 -0700

    update deps

---
+++ elpa/git-commit-20180912.1012/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20180912.1012" "Edit Git commit messages" '((emacs "25.1") (dash "20180413") (with-editor "20180414")) :commit "4daba6973170cd754ad0df7df4dc4e78274222ae" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
