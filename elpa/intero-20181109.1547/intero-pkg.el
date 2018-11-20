42364c4bf792b5dea0afb026fa257da961d8eff3
Michael Xavier <michael@michaelxavier.net>
2018-11-20 11:01:16 -0800

    update deps

---
+++ elpa/intero-20181109.1547/intero-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "intero" "20181109.1547" "Complete development mode for Haskell" '((flycheck "0.25") (company "0.8") (emacs "24.4") (haskell-mode "13.0")) :commit "4be2a4a5de81bae504654a6b3a5d8a340be00e7e" :keywords '("haskell" "tools") :authors '(("Chris Done" . "chrisdone@fpcomplete.com")) :maintainer '("Chris Done" . "chrisdone@fpcomplete.com") :url "https://github.com/commercialhaskell/intero")
