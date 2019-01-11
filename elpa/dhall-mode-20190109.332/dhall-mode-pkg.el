13ec9854c41ef1e1e0890eb5875ace51ad18afa1
Michael Xavier <michael@michaelxavier.net>
2019-01-11 08:31:03 -0800

    update deps

---
+++ elpa/dhall-mode-20190109.332/dhall-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dhall-mode" "20190109.332" "a major mode for dhall configuration language" '((emacs "24.4")) :commit "764a2508a2ab6eca9611d06c5fc10d4a7eef44e3" :keywords '("languages") :authors '(("Sibi Prabakaran" . "sibi@psibi.in")) :maintainer '("Sibi Prabakaran" . "sibi@psibi.in") :url "https://github.com/psibi/dhall-mode")
