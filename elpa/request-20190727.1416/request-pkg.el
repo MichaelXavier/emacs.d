e51e75723dcb2779ec7422b536c17a454f6ca13a
Michael Xavier <michael@michaelxavier.net>
2019-07-29 08:56:25 -0700

    update deps

---
+++ elpa/request-20190727.1416/request-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "request" "20190727.1416" "Compatible layer for URL request in Emacs" '((emacs "24.4")) :commit "6888892644e42860ab0d4daf82943a0d012442f3" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
