f30c110f1b387deb4b390554a474df23db15424e
Michael Xavier <michael@michaelxavier.net>
2019-09-20 13:02:28 -0700

    update deps

---
+++ elpa/dhall-mode-20190919.2242/dhall-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dhall-mode" "20190919.2242" "a major mode for dhall configuration language" '((emacs "24.4") (reformatter "0.3")) :commit "57001a69917329e7933d80a7e21116fe11255ae7" :keywords '("languages") :authors '(("Sibi Prabakaran" . "sibi@psibi.in")) :maintainer '("Sibi Prabakaran" . "sibi@psibi.in") :url "https://github.com/psibi/dhall-mode")
---
+++ elpa/request-20190920.1649/request-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "request" "20190920.1649" "Compatible layer for URL request in Emacs" '((emacs "24.4")) :commit "b074594a8eafe8fbd2f97687ac396900e2fe57b9" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
