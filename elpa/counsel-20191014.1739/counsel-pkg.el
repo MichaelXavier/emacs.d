de25daab7580b90efbe613bc14634b60923841d3
Michael Xavier <michael@michaelxavier.net>
2019-10-15 08:28:00 -0700

    Update deps

---
+++ elpa/counsel-20191014.1739/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20191014.1739" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.12.0")) :commit "c69deb264959ac130ada0a508fc041917ea56c09" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
--- elpa/swiper-20191007.1521/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20191007.1521" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.12.0")) :commit "881cbc516a7355226731f6772b5284fd12c55c1c" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
