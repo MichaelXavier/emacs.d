de4cb88d0839a14d61119bc9270e98034d4dbedc
Michael Xavier <michael@michaelxavier.net>
2019-11-25 15:06:48 -0800

    update deps

--- elpa/haskell-mode-20191110.2148/haskell-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "haskell-mode" "20191110.2148" "A Haskell editing mode"
-  '((emacs "24.4"))
-  :keywords
-  '("haskell" "cabal" "ghc" "repl")
-  :url "https://github.com/haskell/haskell-mode")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
