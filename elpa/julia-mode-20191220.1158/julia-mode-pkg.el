e1bd502d3142bdf5cd72c0042de8150b861356d6
Michael Xavier <michael@michaelxavier.net>
2019-12-20 17:02:44 -0800

    update deps

---
+++ elpa/julia-mode-20191220.1158/julia-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "julia-mode" "20191220.1158" "Major mode for editing Julia source code" 'nil :keywords
+  '("languages")
+  :url "https://github.com/JuliaLang/julia")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
