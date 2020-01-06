7473b65631db973175ebb0bb51f7fe5183a31149
Michael Xavier <michael@michaelxavier.net>
2020-01-06 08:28:05 -0800

    update deps

---
+++ elpa/better-defaults-20200101.2257/better-defaults-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "better-defaults" "20200101.2257" "Fixing weird quirks and poor defaults" 'nil :commit "e051c5278f71af8414f8c6101695197dd4365cca" :keywords '("convenience") :authors '(("Phil Hagelberg")) :maintainer '("Phil Hagelberg") :url "https://github.com/technomancy/better-defaults")
