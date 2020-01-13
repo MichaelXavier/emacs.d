8318287f57e260ca9cd3778ecc9b70078893bed9
Michael Xavier <michael@michaelxavier.net>
2020-01-13 08:40:56 -0800

    update deps

--- elpa/ob-restclient-20191119.924/ob-restclient-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ob-restclient" "20191119.924" "org-babel functions for restclient-mode" '((restclient "0")) :commit "fa4ac671756c9d4e395bd481405239d06b202bae" :keywords '("literate programming" "reproducible research") :authors '(("Alf Lervåg")) :maintainer '("Alf Lervåg") :url "https://github.com/alf/ob-restclient.el")
