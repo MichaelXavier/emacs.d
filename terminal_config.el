d43e1ec6236b8607b0b9474c83fff99566873f0b
Michael Xavier <michael@michaelxavier.net>
2020-03-04 18:43:18 -0800

    Drop some multi-term stuff

--- terminal_config.el
+++ terminal_config.el
@@ -1,25 +1,2 @@
-(require 'multi-term)
 (require 'tramp)
-;; shell-mode
-(defun sh ()
-  (interactive)
-  (ansi-term "/bin/zsh"))
-
-(setq multi-term-program "/bin/zsh")
-(setq multi-term-program-switches "--login")
-
-;; Some settings for term mode
-(add-hook 'term-mode-hook
-          (lambda ()
-            (setq term-buffer-maximum-size 10000)
-            (setq show-trailing-whitespace nil)))
-
-;; Term mode keybindings
-(add-hook 'term-mode-hook
-          (lambda ()
-            (add-to-list 'term-bind-key-alist '("M-[" . multi-term-prev))
-            (add-to-list 'term-bind-key-alist '("M-]" . multi-term-next))
-            (define-key term-raw-map (kbd "C-y") 'term-paste)))
-
-(global-set-key (kbd "C-c t") 'multi-term)
 (define-key global-map (kbd "C-c s") 'counsel-tramp)
