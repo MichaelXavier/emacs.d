4b5392a17e52d995303b28db79bd38d4ece7edf9
Michael Xavier <michael@michaelxavier.net>
2020-03-19 11:10:45 -0700

    update deps

--- elpa/swiper-20200314.1959/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20200314.1959" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "8fae568daafdc79d4990ad739bac42ee230d3234" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
