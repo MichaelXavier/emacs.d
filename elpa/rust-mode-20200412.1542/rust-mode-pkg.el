1aa06357c89a632f3be37ad0b8e6464a54deeb1b
Michael Xavier <michael@michaelxavier.net>
2020-04-13 08:39:01 -0700

    update deps

---
+++ elpa/rust-mode-20200412.1542/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20200412.1542" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "e04e485b9f74b0fcb52816fb01845af083213bdf" :keywords '("languages") :authors '(("Mozilla")) :maintainer '("Mozilla") :url "https://github.com/rust-lang/rust-mode")
---
+++ elpa/yasnippet-20200412.2350/yasnippet-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "yasnippet" "20200412.2350" "Yet another snippet extension for Emacs" '((cl-lib "0.5")) :commit "1cc1996074bdd8fa4e94a51171f5f4ae35f6600b" :keywords '("convenience" "emulation") :maintainer '("Noam Postavsky" . "npostavs@gmail.com") :url "http://github.com/joaotavora/yasnippet")
