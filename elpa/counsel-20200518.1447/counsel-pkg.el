a460f81c1ebea9ccc49131f7152bc95b8c7fd13e
Michael Xavier <michael@michaelxavier.net>
2020-05-19 08:59:00 -0700

    update deps

---
+++ elpa/counsel-20200518.1447/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20200518.1447" "Various completion functions using Ivy" '((emacs "24.5") (swiper "0.13.0")) :commit "7fd0662c49b163b3733e84c2ee3977898c0beaf4" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
