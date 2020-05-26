068c0af6eb59ae20fd0478c3dfc43afdfabdfffa
Michael Xavier <michael@michaelxavier.net>
2020-05-26 09:20:27 -0700

    update deps

--- elpa/counsel-projectile-20200520.1422/counsel-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "counsel-projectile" "20200520.1422" "Ivy integration for Projectile" '((counsel "0.13.0") (projectile "2.0.0")) :commit "1d0b7d22d7ab83fde4da34316f8275daea32d8f7" :keywords '("project" "convenience") :authors '(("Eric Danan")) :maintainer '("Eric Danan") :url "https://github.com/ericdanan/counsel-projectile")
