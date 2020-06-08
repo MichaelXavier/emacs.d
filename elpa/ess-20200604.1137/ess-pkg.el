229963482c27fc543128d53fd90b243285276072
Michael Xavier <michael@michaelxavier.net>
2020-06-08 15:50:15 -0700

    update deps

---
+++ elpa/ess-20200604.1137/ess-pkg.el
@@ -0,0 +1,22 @@
+(define-package "ess" "20200604.1137" "Emacs Speaks Statistics"
+  '((emacs "25.1")
+    (julia-mode "0.3"))
+  :commit "4189b3fd5c960fa22dd7ab05cce95313b619c358" :authors
+  '(("David Smith" . "dsmith@stats.adelaide.edu.au")
+    ("A.J. Rossini" . "blindglobe@gmail.com")
+    ("Richard M. Heiberger" . "rmh@temple.edu")
+    ("Kurt Hornik" . "Kurt.Hornik@R-project.org")
+    ("Martin Maechler" . "maechler@stat.math.ethz.ch")
+    ("Rodney A. Sparapani" . "rsparapa@mcw.edu")
+    ("Stephen Eglen" . "stephen@gnu.org")
+    ("Sebastian P. Luque" . "spluque@gmail.com")
+    ("Henning Redestig" . "henning.red@googlemail.com")
+    ("Vitalie Spinu" . "spinuvit@gmail.com")
+    ("Lionel Henry" . "lionel.hry@gmail.com")
+    ("J. Alexander Branham" . "alex.branham@gmail.com"))
+  :maintainer
+  '("ESS Core Team" . "ESS-core@r-project.org")
+  :url "https://ess.r-project.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
