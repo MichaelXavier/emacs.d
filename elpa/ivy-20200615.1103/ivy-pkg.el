e35a5330f6d05ee75218b7567a87e1a6b06240f6
Michael Xavier <michael@michaelxavier.net>
2020-06-15 08:28:14 -0700

    update deps

---
+++ elpa/ivy-20200615.1103/ivy-pkg.el
@@ -0,0 +1,12 @@
+(define-package "ivy" "20200615.1103" "Incremental Vertical completYon"
+  '((emacs "24.5"))
+  :commit "a007ba637d6c6e232fb894e10a40cfc1142a24ae" :keywords
+  '("matching")
+  :authors
+  '(("Oleh Krehel" . "ohwoeowho@gmail.com"))
+  :maintainer
+  '("Oleh Krehel" . "ohwoeowho@gmail.com")
+  :url "https://github.com/abo-abo/swiper")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-mode-20200615.1353/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20200615.1353" "LSP mode"
+  '((emacs "25.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "414a40ab81e2dbae78c55eac904b69983ddfaf74" :keywords
+  '("languages")
+  :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
