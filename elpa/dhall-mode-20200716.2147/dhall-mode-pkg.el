6b73979ff59f423474ab8da898f188c1fee577b3
Michael Xavier <michael@michaelxavier.net>
2020-07-20 08:23:42 -0700

    update deps

---
+++ elpa/dhall-mode-20200716.2147/dhall-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dhall-mode" "20200716.2147" "a major mode for dhall configuration language" '((emacs "24.4") (reformatter "0.3")) :commit "484bcf8f78f1183ef45c3b04a362bb73690c6b9b" :keywords '("languages") :authors '(("Sibi Prabakaran" . "sibi@psibi.in")) :maintainer '("Sibi Prabakaran" . "sibi@psibi.in") :url "https://github.com/psibi/dhall-mode")
---
+++ elpa/psc-ide-20200702.1540/psc-ide-pkg.el
@@ -0,0 +1,22 @@
+(define-package "psc-ide" "20200702.1540" "Minor mode for PureScript's IDE server."
+  '((emacs "25")
+    (dash "2.13.0")
+    (dash-functional "1.2.0")
+    (company "0.8.7")
+    (s "1.10.0")
+    (flycheck "0.24")
+    (let-alist "1.0.4")
+    (seq "1.11"))
+  :commit "663f4e2cf9cbafdd4b9a60c34346596e2a40c87c" :keywords
+  '("languages")
+  :authors
+  '(("Erik Post" . "erik@shinsetsu.nl")
+    ("Dmitry Bushenko" . "d.bushenko@gmail.com")
+    ("Christoph Hegemann" . "christoph.hegemann1337@gmail.com")
+    ("Brian Sermons"))
+  :maintainer
+  '("Erik Post" . "erik@shinsetsu.nl")
+  :url "https://github.com/purescript-emacs/psc-ide-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
