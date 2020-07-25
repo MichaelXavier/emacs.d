1a42aae07560c68b45ff4986b47816070087beba
Michael Xavier <michael@michaelxavier.net>
2020-07-25 12:32:45 -0700

    Fix deleted buffer issue
    Seems to come from forge. I'm sure I just didn't set it up on this
    machine but I'm also not really using it so I'm happy to shed it.

--- elpa/ghub-20200725.1419/ghub-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "ghub" "20200725.1419" "Minuscule client libraries for Git forge APIs."
-  '((emacs "25.1")
-    (let-alist "1.0.5")
-    (treepy "0.1.1"))
-  :commit "3611f42ba4325eb2b21db7f7b10506b07e3f0999")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- general_config.el
+++ general_config.el
@@ -16,7 +16,6 @@
 (require 'swiper)
 (require 'emojify)
 (require 'magit)
-(require 'forge)
 ;; when its bright out
 ;; (require 'solarized-theme)
 
