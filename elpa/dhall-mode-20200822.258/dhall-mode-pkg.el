4307a64128ca16a98e7325c5c3281703a8459c2c
Michael Xavier <michael@michaelxavier.net>
2020-08-27 09:33:11 -0700

    update deps

---
+++ elpa/dhall-mode-20200822.258/dhall-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dhall-mode" "20200822.258" "Major mode for the dhall configuration language" '((emacs "24.4") (reformatter "0.3")) :commit "97524f934556d561c6c9268ed719f027ed9f55e9" :keywords '("languages") :authors '(("Sibi Prabakaran" . "sibi@psibi.in")) :maintainer '("Sibi Prabakaran" . "sibi@psibi.in") :url "https://github.com/psibi/dhall-mode")
--- elpa/w3m-20200813.37/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20200813.37" "an Emacs interface to w3m" 'nil :commit "a6699824c7d2a33a57bed9e6d21b5a998e226e5d" :keywords
-  '("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
