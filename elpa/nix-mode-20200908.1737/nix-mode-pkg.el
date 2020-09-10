7bfa679c51ebf46e7e98e92a990ab17009bb0771
Michael Xavier <michael@michaelxavier.net>
2020-09-10 08:32:52 -0700

    update deps

---
+++ elpa/nix-mode-20200908.1737/nix-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "nix-mode" "20200908.1737" "Major mode for editing .nix files"
+  '((emacs "25.1"))
+  :commit "91e1a05ea688445fbdaade4f4d7b0d54439c3cd0" :keywords
+  '("nix" "languages" "tools" "unix")
+  :maintainer
+  '("Matthew Bauer" . "mjbauer95@gmail.com")
+  :url "https://github.com/NixOS/nix-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/w3m-20200909.555/w3m-pkg.el
@@ -0,0 +1,5 @@
+(define-package "w3m" "20200909.555" "an Emacs interface to w3m" 'nil :commit "63c2c078ed358e5a11a0e1e6f3fda250d4ec5c4c" :keywords
+  '("w3m" "www" "hypermedia"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
