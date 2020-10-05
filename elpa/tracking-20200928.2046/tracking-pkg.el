660ac779998e0d8c19abac61bb1fbd4efe71dbcd
Michael Xavier <michael@michaelxavier.net>
2020-10-05 08:25:52 -0700

    update deps

---
+++ elpa/tracking-20200928.2046/tracking-pkg.el
@@ -0,0 +1,8 @@
+(define-package "tracking" "20200928.2046" "Buffer modification tracking" 'nil :commit "d98986ce933c380b47d727beea8bad81bda65dc9" :authors
+  '(("Jorgen Schaefer" . "forcer@forcix.cx"))
+  :maintainer
+  '("Jorgen Schaefer" . "forcer@forcix.cx")
+  :url "https://github.com/jorgenschaefer/circe/wiki/Tracking")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
