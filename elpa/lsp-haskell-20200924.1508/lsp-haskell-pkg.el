e9f62fa52863849dbae4e17a649e9ca2df284124
Michael Xavier <michael@michaelxavier.net>
2020-10-19 08:23:53 -0700

    update deps

--- elpa/lsp-haskell-20200924.1508/lsp-haskell-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/michael/.emacs.d/elpa/lsp-haskell-20200924.1508/lsp-haskell.el  -*- no-byte-compile: t -*-
-(define-package "lsp-haskell" "20200924.1508" "Haskell support for lsp-mode" '((lsp-mode "3.0") (haskell-mode "1.0")) :commit "a56667b496e5370f1a50310589a2d2a4d3b9d11e" :keywords '("haskell") :url "https://github.com/emacs-lsp/lsp-haskell")
