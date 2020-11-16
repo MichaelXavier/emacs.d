a8b94f64ad3f7e7e815536695c1318b07175162f
Michael Xavier <michael@michaelxavier.net>
2020-11-16 12:30:29 -0800

    update deps

---
+++ elpa/ess-20201115.1122/ess-pkg.el
@@ -0,0 +1,21 @@
+(define-package "ess" "20201115.1122" "Emacs Speaks Statistics"
+  '((emacs "25.1"))
+  :commit "44c0ddb7157dea682e157bf18ef88e8990c4694b" :authors
+  (("David Smith" . "dsmith@stats.adelaide.edu.au")
+   ("A.J. Rossini" . "blindglobe@gmail.com")
+   ("Richard M. Heiberger" . "rmh@temple.edu")
+   ("Kurt Hornik" . "Kurt.Hornik@R-project.org")
+   ("Martin Maechler" . "maechler@stat.math.ethz.ch")
+   ("Rodney A. Sparapani" . "rsparapa@mcw.edu")
+   ("Stephen Eglen" . "stephen@gnu.org")
+   ("Sebastian P. Luque" . "spluque@gmail.com")
+   ("Henning Redestig" . "henning.red@googlemail.com")
+   ("Vitalie Spinu" . "spinuvit@gmail.com")
+   ("Lionel Henry" . "lionel.hry@gmail.com")
+   ("J. Alexander Branham" . "alex.branham@gmail.com"))
+  :maintainer
+  ("ESS Core Team" . "ESS-core@r-project.org")
+  :url "https://ess.r-project.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/lsp-ivy-20200621.1459/lsp-ivy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "lsp-ivy" "20200621.1459" "LSP ivy integration" '((emacs "25.1") (dash "2.14.1") (lsp-mode "6.2.1") (ivy "0.13.0")) :commit "1f6d8777a3445d23678216fe1f46475de5f4fb0e" :keywords '("languages" "debug") :url "https://github.com/emacs-lsp/lsp-ivy")
