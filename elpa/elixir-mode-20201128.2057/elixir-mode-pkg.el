32784bc9cd79a2d4c79d3cb9960f51709c02a5ce
Michael Xavier <michael@michaelxavier.net>
2020-11-30 08:29:56 -0800

    update deps

---
+++ elpa/elixir-mode-20201128.2057/elixir-mode-pkg.el
@@ -0,0 +1,9 @@
+(define-package "elixir-mode" "20201128.2057" "Major mode for editing Elixir files"
+  '((emacs "24")
+    (pkg-info "0.4"))
+  :commit "b78e7f733be7bf2af35807d759e53b7d52323193" :keywords
+  ("languages" "elixir")
+  :url "https://github.com/elixir-editors/emacs-elixir")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
