9cf3fdc95fe66230838cad33448cf90b086a240c
Michael Xavier <michael@michaelxavier.net>
2021-01-04 13:16:56 -0800

    update deps

--- elpa/dash-functional-20201215.40/dash-functional-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/michael/.emacs.d/elpa/dash-functional-20201215.40/dash-functional.el  -*- no-byte-compile: t -*-
-(define-package "dash-functional" "20201215.40" "Collection of useful combinators for Emacs Lisp" '((dash "2.0.0") (emacs "24")) :commit "f800e2e6537f0a16a4041f488b14f10706a61dff" :keywords '("extensions" "lisp"))
--- elpa/w3m-20201224.4/w3m-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "w3m" "20201224.4" "an Emacs interface to w3m" 'nil :commit "085de54acae027e7f0d0b9bf34d3142993234fd3" :keywords
-  ("w3m" "www" "hypermedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
