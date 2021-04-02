c91427b5a3cecf3bc3f516ebb0e49a0a2acc61e4
Michael Xavier <michael@michaelxavier.net>
2021-04-02 13:12:06 -0700

    Update deps

---
+++ elpa/evil-20210401.1045/evil-pkg.el
@@ -0,0 +1,10 @@
+(define-package "evil" "20210401.1045" "Extensible Vi layer for Emacs."
+  '((emacs "24.1")
+    (goto-chg "1.6")
+    (cl-lib "0.5"))
+  :commit "80e9e918927dd87a19fd69af31fe35f27ca67a8f" :keywords
+  '("emulation" "vim")
+  :url "https://github.com/emacs-evil/evil")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
