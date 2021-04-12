eaee4dce260ba72109c746e082082e4f55a2801a
Michael Xavier <michael@michaelxavier.net>
2021-04-12 08:51:25 -0700

    Update deps

--- elpa/lsp-ivy-20210320.913/lsp-ivy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /home/michael/.emacs.d/elpa/lsp-ivy-20210320.913/lsp-ivy.el  -*- no-byte-compile: t -*-
-(define-package "lsp-ivy" "20210320.913" "LSP ivy integration" '((emacs "25.1") (dash "2.14.1") (lsp-mode "6.2.1") (ivy "0.13.0")) :commit "515e5977b3d1f6cb521984f084868f28efd47e72" :keywords '("languages" "debug") :url "https://github.com/emacs-lsp/lsp-ivy")
