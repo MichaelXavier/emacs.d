f12c25c9d5d3c8e893a0771992f99952ce37436f
Michael Xavier <michael@michaelxavier.net>
2021-04-19 08:48:57 -0700

    Update deps

---
+++ elpa/tao-theme-20210417.626/tao-theme-pkg.el
@@ -0,0 +1,8 @@
+(define-package "tao-theme" "20210417.626" "This package provides two parametrized uncoloured color themes for Emacs: tao-yin and tao-yang." 'nil :commit "d5ccf6f53d65e80083acdfb0bced6bcd678c6ea9" :authors
+  '(("Peter Kosov" . "11111000000@email.com"))
+  :maintainer
+  '("Peter Kosov" . "11111000000@email.com")
+  :url "http://github.com/11111000000/tao-theme-emacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
